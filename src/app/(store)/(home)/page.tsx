0a1e9635a2ddffd7535585f42acb9e340823b8c5
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-15 15:35:48 -0300

    feat: add cache

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 import Link from "next/link";
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api("/api/products/featured");
-
+  const response = await api("/api/products/featured", {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  });
   const products = await response.json();
 
   return products;
