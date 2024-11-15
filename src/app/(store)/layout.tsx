54bf3b7be5dd1c5c41fa90a325843c015f8fdd35
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-15 13:42:22 -0300

    feat: add source

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@/components/header";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function StoreLayout({ children }: Readonly<Props>) {
+  return (
+    <div className="mx-auto grid min-h-screen max-w-[1600px] grid-rows-app gap-5 p-8">
+      <Header />
+      {children}
+    </div>
+  );
+}
