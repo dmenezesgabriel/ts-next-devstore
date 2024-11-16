f3a43b757a60064a297ba42deaa938f94d969059
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-16 19:56:02 -0300

    feat: add search functionality

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,22 +1,50 @@
+import { redirect } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import { api } from "../../../data/api";
+import { Product } from "../../../data/product";
 
-export default async function Search() {
+interface SearchProps {
+  searchParams: Promise<{
+    q: string;
+  }>;
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/api/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  });
+  const products = await response.json();
+
+  return products;
+}
+
+export default async function Search({ searchParams }: SearchProps) {
+  const { q: query } = await searchParams;
+
+  if (!query) {
+    redirect("/");
+  }
+
+  const products = await searchProducts(query);
+
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
-        Resultados para: <span className="font-semibold">moletom</span>
+        Resultados para: <span className="font-semibold">{query}</span>
       </p>
 
       <div className="grid grid-cols-3 gap-6">
-        {Array.from({ length: 3 }).map((item, index) => (
+        {products.map((product) => (
           <Link
-            key={index}
-            href={`/product/moletom-never-stop-learning`}
+            key={product.id}
+            href={`/product/${product.slug}`}
             className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
           >
             <Image
-              src="/moletom-never-stop-learning.png"
+              src={product.image}
               className="group-hover:scale-105 transition-transform duration-500"
               alt=""
               width={480}
@@ -24,11 +52,9 @@ export default async function Search() {
               quality={100}
             />
             <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-              <span className="text-sm truncate">
-                Moletom Never Stop Learning
-              </span>
+              <span className="text-sm truncate">{product.title}</span>
               <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-                {new Number(99.0).toLocaleString("pt-BR", {
+                {product.price.toLocaleString("pt-BR", {
                   style: "currency",
                   currency: "BRL",
                   maximumFractionDigits: 0,
