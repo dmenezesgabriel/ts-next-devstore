6a2993db7c9ff3216a1b29a69b2abcfcd6ace14a
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-16 09:24:44 -0300

    feat: add home loading

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,8 @@
 import data from "../data.json";
 
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Delay to simulate real api
+
   const featuredProducts = data.products.filter((product) => product.featured);
 
   return Response.json(featuredProducts);
