de98527545b71964b7b4306ce590f4b8ba099f60
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-16 11:31:47 -0300

    feat: add product data fetching

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,11 +1,35 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/product";
 import Image from "next/image";
 
-export default function ProductPage() {
+interface ProductProps {
+  params: { slug: string };
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  console.log(`/api/products/${slug}`);
+
+  const response = await api(`/api/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  });
+
+  const product = await response.json();
+
+  return product;
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+  const { slug } = await params; // next.js v14 => params.slug (no await needed)
+
+  const product = await getProduct(slug);
+
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/moletom-never-stop-learning.png"
+          src={product.image}
           alt=""
           width={1000}
           height={1000}
@@ -13,19 +37,26 @@ export default function ProductPage() {
         />
       </div>
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">
-          Moletom never stop learning
-        </h1>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodão e 12% de poliester.
+          {product.description}
         </p>
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            R$129
+            {product.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+              maximumFractionDigits: 0,
+              minimumFractionDigits: 0,
+            })}
           </span>
           <span className="text-sm text-zinc-400">
-            Em 12x s/juros de R$10,75
+            Em 12x s/juros de{" "}
+            {(product.price / 12).toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
           </span>
         </div>
 
---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,26 @@
+import data from "../data.json";
+import { z } from "zod";
+
+export async function GET(
+  _: Request,
+  { params }: { params: { slug: string } }
+) {
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Delay to simulate real api
+
+  const { slug } = await params; // next.js v14 => params.slug (no await needed)
+
+  const parsedSlug = z.string().parse(slug);
+
+  const product = data.products.find((product) => product.slug === parsedSlug);
+
+  if (!product) {
+    return Response.json(
+      { message: "Product not found" },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  return Response.json(product);
+}
