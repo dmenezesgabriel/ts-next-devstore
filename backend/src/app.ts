1843b496e991bc3198fbed904b772b0b7ba46a28
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-17 14:28:12 -0300

    chore: update app to serverless deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,20 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { productRoutes } from "./routes/products";
 
-export const app = fastify();
+export const app = fastify({ logger: true });
 
 app.get("/", async () => {
   return { message: "Hello, World!" };
 });
 
 app.register(productRoutes, { prefix: "products" });
+
+export default async (req: FastifyRequest, res: FastifyReply) => {
+  try {
+    await app.ready();
+    app.server.emit("request", req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+};
