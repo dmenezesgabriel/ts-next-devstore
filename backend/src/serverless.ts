0cd97f62039a0522b4219df34b6b803d5a6414ef
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-17 19:07:26 -0300

    fix: vercel deployment

--- backend/src/serverless.ts
+++ backend/src/serverless.ts
@@ -1,12 +1,12 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { app } from "./app";
 
-export default async (req: FastifyRequest, res: FastifyReply) => {
+export default async (request: FastifyRequest, reply: FastifyReply) => {
   try {
     await app.ready();
-    app.server.emit("request", req, res);
+    app.server.emit("request", request, reply);
   } catch (error) {
     console.error(error);
-    res.status(500).send({ error: "Internal Server Error" });
+    reply.status(500).send({ error: "Internal Server Error" });
   }
 };
