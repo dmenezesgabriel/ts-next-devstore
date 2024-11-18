b8ab44da354d821887e689aad3b6a39deffbac3d
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-11-17 22:29:35 -0300

    fix: client boundaries

--- frontend/src/app/(store)/layout.tsx
+++ frontend/src/app/(store)/layout.tsx
@@ -1,7 +1,6 @@
 import { CartProvider } from "../../contexts/cart-context";
 import { Header } from "../../components/header";
 import { ReactNode } from "react";
-import { Suspense } from "react";
 
 interface Props {
   children: ReactNode;
@@ -11,10 +10,8 @@ export default function StoreLayout({ children }: Readonly<Props>) {
   return (
     <CartProvider>
       <div className="mx-auto grid min-h-screen max-w-[1600px] grid-rows-app gap-5 p-8">
-        <Suspense>
-          <Header />
-          {children}
-        </Suspense>
+        <Header />
+        {children}
       </div>
     </CartProvider>
   );
--- frontend/src/app/(store)/search/loading.tsx
+++ frontend/src/app/(store)/search/loading.tsx
@@ -1,19 +1,13 @@
-"use client";
-
 import { Skeleton } from "../../../components/skeleton";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import { CurrentSearch } from "./current-search";
 
 export default function SearchLoading() {
-  const searchParams = useSearchParams();
-
-  const query = searchParams.get("q");
-
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query ?? ""}</span>
-      </p>
-
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
       <div className="grid grid-cols-3 gap-6">
         {Array.from({ length: 6 }).map((_, index) => (
           <Skeleton key={index} className="h-[400px]" />
